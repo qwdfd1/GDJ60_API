c4a9cf1ec96c0cffac48add2a3c17bdd688b1ce3
qwdfd1 <qwdfd@naver.com>
2023-01-05 17:41:12 +0900

    0105_String

---
+++ src/com/onion/api1/strings/StringStudy3.java
@@ -0,0 +1,26 @@
+package com.onion.api1.strings;
+
+public class StringStudy3 {
+	public static void main(String[] args) {
+		
+		// String은 불변
+		// 1,2는 상수구역에서 찾으면서 같으면 같은 주소값을 준다. 그래서 true
+		// 3,4는 new키워드로 객체생성, 주소값을 비교한다. 그래서 false
+		String str1 = "hello";
+		String str2 = "hello";
+		String str3 = new String("hello");
+		String str4 = new String("hello");
+		
+		System.out.println(str1==str2);
+		System.out.println(str3==str4);
+		System.out.println(str1==str3);
+		
+
+		// String은 연산시에는 연산할 때 마다 새로운 객체를 생성한다
+		// 연산을 마치고나면 최종객체의 주소값을 반환해준다. 
+		str1 = str1+"world"+str2;
+		System.out.println(str1);
+
+		
+	}
+}
