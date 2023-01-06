88ed1b47cda3a27fc3845122517d6c2234051ded
qwdfd1 <qwdfd@naver.com>
2023-01-06 11:00:17 +0900

    String Ex2

--- src/com/onion/api1/strings/StringEx2.java
+++ src/com/onion/api1/strings/StringEx2.java
@@ -7,6 +7,7 @@ public class StringEx2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("프로그램 시작");
 
 		Scanner sc = new Scanner(System.in);
 		String [] names = {"jpg", "jpeg", "gif", "png"};
@@ -14,52 +15,57 @@ public class StringEx2 {
 		System.out.println("파일명을 입력해주세요");
 		System.out.println("FileName  : ");
 		String fileName = sc.next();
-		boolean check = false;
+		boolean check = true;
 		
-		// 첫번째 방법
+		// 1번째 방법
+		// 배열에 담긴 이미지타입과 같은지 검사
+		// 반환값이 -1이 아닐때
 		for(int i=0; i<names.length; i++) {
 			if(fileName.lastIndexOf(names[i]) != -1) {
-				check = true;
+				System.out.println("확인");
+				System.out.println("FileName : " + fileName);
+				check = false;
 				break;
 			}
 		}
+		
+		// 잘못됐을 때
 		if(check) {
-			System.out.println("확인");
-			System.out.println("FileName : " + fileName);
-		}
-		else {
 			System.out.println("잘못된 파일형식입니다");
 			System.out.println("FileName : " + fileName);
 		}
 		
+		System.out.println("----------------------------------------------------");
+
+		
 		//2번째 방법
+		check = true;
 		System.out.println("파일명을 입력해주세요");
 		System.out.println("FileName  : ");
 		fileName = sc.next();
 		
+		// 확장자 분리
 		String result = fileName.substring(fileName.lastIndexOf('.')+1);
+		
+		// 확장자 검증
 		for(int i=0; i<names.length; i++) {
 			if(result.equals(names[i])) {
-				check = true;
-			}
-			else {
-				check = false;
-			}
-			}
-		
-			if(check) {
 				System.out.println("확인");
 				System.out.println("FileName : " + fileName);
-				
-			}
-			
-			else {
-				System.out.println("잘못된 파일형식입니다");
-				System.out.println("FileName : " + fileName);
+				check = false;
+				break;
+				}
 			}
 		
+		//비정상 파일타입 일 때
+		if(check) {
+			System.out.println("비정상 파일");
+			System.out.println("FileName : " + fileName);
+		}
 		
 		
+		
+		System.out.println("프로그램 종료");
 	}
 
 }
