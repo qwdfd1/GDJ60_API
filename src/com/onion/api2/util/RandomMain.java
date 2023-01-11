4f3196ea1a75d70f55dd7064b4c217b66526872e
qwdfd1 <qwdfd@naver.com>
2023-01-11 10:08:53 +0900

    Collection(Lotto)

--- src/com/onion/api2/util/RandomMain.java
+++ src/com/onion/api2/util/RandomMain.java
@@ -13,25 +13,73 @@ public class RandomMain {
 		
 		//1000원 로또 구매 1-45
 		//6개의 숫자 출력
-		HashSet<Integer> fuck = new HashSet<>();
+		
+//		 Set
+		HashSet<Integer> set = new HashSet<>();
 		
 		int num = 0;
 		
 		boolean check1 = true;
-		while(check1) {
-			num = random.nextInt(1,45);
-			fuck.add(num);
-			if(fuck.size() == 6) {
-				check1 = false;
-			}
-		}
-
-		Iterator<Integer> fuckkkkkk = fuck.iterator();
-		while(fuckkkkkk.hasNext()) {
-			System.out.print(fuckkkkkk.next() + "\t");
+		while(set.size() < 6) {
+			set.add(random.nextInt(1,46));
 		}
 		
+		System.out.println(set);
 
+
+		
+
+		
+//		//1. 배열
+//		int [] nums = new int[6];
+//		
+//		for(int i=0; i<nums.length; i++) {
+//			nums[i] = random.nextInt(1,46);
+//			for(int j=0; j<i; j++) {
+//				if(nums[i] == nums[j]) {
+//					i--;
+//					break;
+//				}
+//			}
+//		}
+//		
+//		for(int i=0; i<nums.length; i++) {
+//			System.out.println(nums[i]);
+//
+//		}
+		
+		//2. ArrayList
+		
+//		ArrayList<Integer> nums = new ArrayList<>();
+//		
+//		
+//		while(nums.size() < 6) {
+//			
+//			boolean check = true;
+//			int num = random.nextInt(1,46);
+//			
+//			for(int i=0; i<nums.size(); i++) {
+//				if(nums.get(i) == num) {
+//					check=false;
+//					break;
+//				}
+//			}
+//					
+//			if(check) {
+//				nums.add(num);
+//			}
+//
+//		}
+//		
+//		for(int i=0; i<nums.size(); i++) {
+//			System.out.println(nums.get(i));
+//		}
+//		
+		
+			
+		
+		
+		
 		
 
 		
