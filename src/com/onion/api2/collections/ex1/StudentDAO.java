889ef35035112bb2006267bc6a094274af6ddbd5
qwdfd1 <qwdfd@naver.com>
2023-01-12 10:50:51 +0900

    StudentDAo-modify

--- src/com/onion/api2/collections/ex1/StudentDAO.java
+++ src/com/onion/api2/collections/ex1/StudentDAO.java
@@ -1,6 +1,8 @@
 package com.onion.api2.collections.ex1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,12 +13,8 @@ import java.util.StringTokenizer;
 public class StudentDAO {
 	private StringBuffer sb;
 	private Scanner sc;
-	private final long now;
-	private Calendar calendar;
 
 	public StudentDAO() {
-		calendar = Calendar.getInstance();
-		now = calendar.getTimeInMillis();
 		sc = new Scanner(System.in);
 		this.sb = new StringBuffer();
 		
@@ -29,69 +27,146 @@ public class StudentDAO {
 	public void saveStudent(ArrayList<StudentDTO> studentDTOs) {
 		Calendar calendar = Calendar.getInstance();
 		long now = calendar.getTimeInMillis();
+		File file = new File("C:\\fileTest", now+".txt");
 		
-		for(int i=0; i<studentDTOs.size(); i++) {
-			StudentDTO studentDTO = studentDTOs.get(i);
-			String str = studentDTO.getName() + "-" + studentDTO.getNum() + "-"  + studentDTO.getKor()
-			 + "-" + studentDTO.getEng() + "-"  + studentDTO.getMath() + "\r\n";
-			File file = new File("C:\\fileTest", now+".txt");
-			try {
-				FileWriter fw = new FileWriter(file, true);
-				fw.write(str);
+		//finally에서도 참조변수를 사용하기위해 try문 밖에서 선언 후 null로 초기화
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter(file);
+			
+			for(StudentDTO studentDTO : studentDTOs) {
+				StringBuffer sb = new StringBuffer();
+				sb.append(studentDTO.getName());
+				sb.append("-");
+				sb.append(studentDTO.getNum());
+				sb.append("-");
+				sb.append(studentDTO.getKor());
+				sb.append("-");
+				sb.append(studentDTO.getEng());
+				sb.append("-");
+				sb.append(studentDTO.getMath());
+				sb.append("\n");
+				
+				fw.write(sb.toString());
 				fw.flush();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fw.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
 		}
+		
+		
+		
 		
 		
 	}
 	
 	public ArrayList<StudentDTO> init() {
 	
-		//문자열 파싱
-		String data = sb.toString();
-
-		data = data.replace(", ", "-");
-		data = data.replace(" ", "-");
 
 		
-		StringTokenizer st = new StringTokenizer(data,"-");
-		ArrayList<StudentDTO> studentDTOs = new ArrayList<>();
+		//1. 읽어올 파일의 부모디렉토리 경로
+		File parent = new File("C:\\fileTest");
 		
-		//각 토큰을 setter로 각 객체멤버에 대입
-		while(st.hasMoreTokens()) {
-			StudentDTO studentDTO = new StudentDTO();
-			
-			String name = st.nextToken();
-			studentDTO.setName(name);
-			
-			int num = Integer.parseInt(st.nextToken());
-			studentDTO.setNum(num);
-			
-			int kor = Integer.parseInt(st.nextToken());
-			studentDTO.setKor(kor);
-			
-			int eng = Integer.parseInt(st.nextToken());
-			studentDTO.setEng(eng);
-			
-			int math = Integer.parseInt(st.nextToken());
-			studentDTO.setMath(math);
-			
+		// 부모경로의 파일리스트
+		String [] fileList = parent.list();
 		
+		// 부모경로의 파일들의 이름을 담을 Long타입 배열
+		Long [] fileNames = new Long[fileList.length];
+		long max = 0;
+	
+		
+		for(int i=0; i<fileList.length; i++) {
 			
-			int total = kor+eng+math;
-			studentDTO.setTotal(total);
+			//파일의 확장자를 잘라내고 long타입으로 변환
+			fileNames[i] = Long.parseLong(fileList[i].substring(0, fileList[i].lastIndexOf('.')));
 			
-			double avg = total/3.0;
-			studentDTO.setAvg(avg);
-
-			studentDTOs.add(studentDTO);
-			
-			
+			//두 수를 비교하고 큰 수를 max변수에 대입
+			if(fileNames[i] > max) {
+				max = fileNames[i];
+			}
 		}
+		
+		
+		//제일 최근 백업파일 읽어오기
+		File file = new File(parent, max+".txt");
+		
+		
+		//2. 파일내용 읽기위해서 연결 준비
+		//다른 구문에서도 사용하기위해 변수 선언후 null로 초기화
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		ArrayList<StudentDTO> studentDTOs = new ArrayList<>();
+
+		
+		
+		try {
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			String data = null;
+			
+			
+			
+			while((data = br.readLine()) != null) {
+
+				//문자열 파싱
+				data = data.replace(", ", "-");
+				data = data.replace(" ", "-");
+				StringTokenizer st = new StringTokenizer(data,"-");
+				
+				//각 토큰을 setter로 각 객체멤버에 대입
+				
+					StudentDTO studentDTO = new StudentDTO();
+					
+					String name = st.nextToken();
+					studentDTO.setName(name);
+					
+					int num = Integer.parseInt(st.nextToken());
+					studentDTO.setNum(num);
+					
+					int kor = Integer.parseInt(st.nextToken());
+					studentDTO.setKor(kor);
+					
+					int eng = Integer.parseInt(st.nextToken());
+					studentDTO.setEng(eng);
+					
+					int math = Integer.parseInt(st.nextToken());
+					studentDTO.setMath(math);
+					
+				
+					
+					int total = kor+eng+math;
+					studentDTO.setTotal(total);
+					
+					double avg = total/3.0;
+					studentDTO.setAvg(avg);
+
+					studentDTOs.add(studentDTO);
+										
+				}
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				fr.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
 		
 		return studentDTOs;
 		
